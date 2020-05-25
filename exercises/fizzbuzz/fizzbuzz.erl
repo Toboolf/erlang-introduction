4895a966ee903d08979a80568a04c0f011c2c408
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-05-25 11:54:15 -0500

    coding master finished fizz buzz no refactor

--- exercises/fizzbuzz/fizzbuzz.erl
+++ exercises/fizzbuzz/fizzbuzz.erl
@@ -1,5 +1,5 @@
 -module(fizzbuzz).
--export([evaluate_number/1]).
+-export([evaluate_number/1, do_fizzbuzz/2]).
 
 evaluate_number(Number) when (Number rem 3 =:= 0) and (Number rem 5 =:= 0) ->
   fizz_buzz;
@@ -13,3 +13,5 @@ evaluate_number(Number) when (Number rem 5) =:= 0 ->
 evaluate_number(Number) ->
   Number.
 
+do_fizzbuzz(Start, End) ->
+  lists:map(fun(Number) -> evaluate_number(Number) end, lists:seq(Start, End)).
