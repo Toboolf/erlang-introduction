6f526a7de36600274b359298c7616406779f6032
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-06-29 12:01:36 -0500

    coding master solving exercise division without arithmetic operators

---
+++ exercises/division/division_recursive.erl
@@ -0,0 +1,23 @@
+-module(division_recursive).
+
+%% Hacer una división sin usar operadores aritméticos sin usar operadores
+divide_numbers(Dividend, Divisor) when (Divisor > 0) ->
+  divide(Dividend, Divisor, 0, 0).
+
+divide(Dividend, Divisor, Sum, Count) when (Sum + Divisor) < Dividend ->
+  io:format("Sum: ~w, Count: ~w~n", [Sum, Count]),
+  divide(Dividend, Divisor, Sum + Divisor, Count + 1);
+
+divide(Dividend, _Divisor, Sum, Count) ->
+  io:format("Final Sum: ~w, Count: ~w~n", [Sum, Count]),
+  {Count, Dividend - Sum}.
+
+%% 10 / 5
+%% c = 1, 5 > 10 si
+%% c = 2, 10 > 10 no-
+%%
+%% 10 / 3
+%% sobra 1
+%% 1 sumarlo 10 = 10
+%% dividir 10 / Divisor = 3 , residuo 1
+
